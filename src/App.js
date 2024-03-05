201e9ffe0f8892ec125bec9a07422b1be6ea0c95
Ghazal Mir <ghazalmirsayah@gmail.com>
2024-03-04 23:15:48 -0500

    Added the form for posting a new ad

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import AdDetails from "./pages/ads/adDetails";
 import PageNotFound from './pages/pageNotFound';
 import LoggedInNavbar from './components/navbars/LoggedInNavbar.js';
 import LoggedOutNavbar from './components/navbars/LoggedOutNavbar.js';
+import NewAdForm from './pages/ads/newAdForm';
+
 // Styling 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
@@ -26,6 +28,8 @@ function App() {
         {/* Add the actual main page once you have it */}
         <Route path="/" element={<AdDetails />} />
 				<Route path="/AdDetails" element={<AdDetails />} />
+				<Route path="/NewAd" element={<NewAdForm />} />
+
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
       {/* Add the componenet once you have it
---
+++ src/pages/ads/newAdForm.js
@@ -0,0 +1,95 @@
+import BlueButton from "../../components/general/blueButton";
+
+var files = [];
+
+function NewAdForm(props) {
+  return (
+    <form className="m-5" action="/newad">
+      <h2>Item Information</h2>
+      <div className="row">
+        {/* Title */}
+        <div className="col col-sm-6 col-12 mb-3">
+          <label htmlFor="title" className="form-label">Title <span className="text-danger">*</span></label>
+          <input type="email" className="form-control bg-body-tertiary" id="title" placeholder="Title" required/>
+        </div>
+        {/* Price */}
+        <div className="col col-sm-6 col-12  mb-3">
+          <label htmlFor="price" className="form-label">Price <span className="text-danger">*</span></label>
+          <div className="input-group">
+          <span className="input-group-text">$</span>
+          <input type="number" className="form-control bg-body-tertiary" id="price" placeholder="Price" min="0" required/>  
+          </div>    
+        </div>
+      </div>
+      {/* Description */}
+      <div className="mb-3">
+        <label htmlFor="description" className="form-label">Description</label>
+        <textarea className="form-control bg-body-tertiary" id="description" placeholder="Lorem ipsum..." rows="3"></textarea>
+      </div>
+      {/* Category */}
+      <div className="mb-3">
+        <label htmlFor="category" className="form-label">Categoy <span className="text-danger">*</span></label>
+        <select id="category" className="form-select bg-body-tertiary" required>
+          <option defaultValue={null}>Select a category</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </div>
+      {/* File upload */}
+      <div className="mb-3">
+        <label htmlFor="formFile" className="form-label">Images <span className="text-danger">*</span></label>
+        <input onChange={(event) => {fileUpload(event);}}
+                className="form-control bg-body-tertiary" type="file" id="formFile" 
+                multiple="multiple" accept=".jpg,.png" required/>
+        <div id="selectedFiles" className="mt-3"></div>
+      </div>
+      <hr className="my-4"></hr>
+      <h2>Meetup Information</h2>
+      <p>Where do you want the transaction to happen?</p>
+      {/* Address */}
+      <div className="mb-3">
+        <label htmlFor="street" className="form-label">Street Address</label>
+        <input type="text" className="form-control bg-body-tertiary" id="street" placeholder="Street Adress" autoComplete="street-address"/>
+      </div>
+      <div className="row mb-4">
+        <div className="col col-sm-6 col-12">
+          <label htmlFor="city" className="form-label">City <span className="text-danger">*</span></label>
+          <input type="text" className="form-control bg-body-tertiary" id="city" placeholder="City" autoComplete="address-level2" required/>
+        </div>
+        <div className="col col-sm-6 col-12">
+          <label htmlFor="country" className="form-label">Country <span className="text-danger">*</span></label>
+          <input type="text" className="form-control bg-body-tertiary" id="country" placeholder="Country" autoComplete="country" required/>
+        </div>
+      </div>
+      {/* Submit/cancel buttons */}
+      <div className="text-end">
+        <input className="btn btn-primary text-white rounded border-0 p-2 px-4" type="submit" value="Post Ad"/>
+        <a type="button" href="/" className="btn p-2 px-4" >Cancel</a>
+      </div>
+    </form>
+  )
+  }
+
+  function fileUpload(event) {
+    if(!event.target.files || !window.FileReader) return;
+    let selDiv = document.querySelector("#selectedFiles");
+    selDiv.innerHTML = "";
+    
+    //var files = ;
+    var filesArr = Array.prototype.slice.call(event.target.files);
+    files.push(...filesArr);
+    files.forEach(function(f) {
+      if(!f.type.match("image.*")) {
+        return;
+      }
+  
+      var reader = new FileReader();
+      reader.onload = function (event) {
+        var html = "<img style='max-height:100px;' class='mt-3' src=\"" + event.target.result + "\">" +"   " + f.name + "<br clear=\"left\"/>";
+        selDiv.innerHTML += html;				
+      }
+      reader.readAsDataURL(f); 
+    });
+  }
+export default NewAdForm;
