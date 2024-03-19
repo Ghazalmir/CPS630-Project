a7c8f3932c5ee552ce0ffb6d15dbc97a5c9af4e4
Rey Aldrin Uy <91858993+reyaldrinuy@users.noreply.github.com>
2024-03-19 03:25:34 -0400

    Fixed navigation links

--- src/pages/admin/ManageUsers.js
+++ src/pages/admin/ManageUsers.js
@@ -13,9 +13,9 @@ function ManageUsers() {
           <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
           <main>
             <section className="links">
-              <a href="./reported_ads.html">Reported Ads</a>
-              <a href="./reported_users.html">Reported Users</a>
-              <a href="./manage_users.html" className="active">Manage Users</a>
+              <a href="./ReportedAds">Reported Ads</a>
+              <a href="./ReportedUsers">Reported Users</a>
+              <a href="./ManageUsers" className="active">Manage Users</a>
             </section>
             <section>
               <form action="#" method="post">
