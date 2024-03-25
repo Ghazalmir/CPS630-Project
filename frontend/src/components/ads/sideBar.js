7f0fc89222a0203acd514c7369dcb61517f2aeed
AngeloDelaCruz <91810126+angelojdelacruz@users.noreply.github.com>
2024-03-25 14:46:30 -0400

    Update sideBar.js

--- frontend/src/components/ads/sideBar.js
+++ frontend/src/components/ads/sideBar.js
@@ -1,36 +1,60 @@
-import React, { useState } from 'react';
-import Dropdown from 'react-bootstrap/Dropdown';
-import './adContainer.css';
+import React, { useEffect, useState } from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import "./adContainer.css";
 
-function sideBar() {
+function SideBar() {
+  const [categories, setCategories] = useState([]);
 
-    return (
-        <div className="container d-flex justify-content-center pt-5 side-bar">
-            <ul className="list-unstyled">
-                <li>
-                    <button className='btn align-items-center category'>
-                        Items Wanted
-                    </button>
-                </li>
-                <li>
-                    <button className='btn align-items-center category'>
-                        Items for Sale
-                    </button>
-                </li>
-                <li>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic" className="sidebar-dropdown category">
-                            Academic Services
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-tutoring">Tutoring</Dropdown.Item>
-                            <Dropdown.Item href="#/action-textbooks">Textbooks</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </li>
-            </ul>
-        </div>
-    );
+  useEffect(() => {
+    // Placeholder
+    fetch("http://localhost:3000/api/categories")
+      .then((response) => response.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((error) => console.error("Error fetching categories:", error));
+  }, []);
+
+  return (
+    <div className="container d-flex justify-content-center pt-5 side-bar">
+      <ul className="list-unstyled">
+        {categories.map((category, index) => (
+          <li key={index}>
+            {category.subCategories ? (
+              <Dropdown>
+                <Dropdown.Toggle
+                  variant="success"
+                  id={`dropdown-${category.id}`}
+                  className="sidebar-dropdown category"
+                >
+                  {category.name}
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  {category.subCategories.map((subCategory, subIndex) => (
+                    <Dropdown.Item
+                      key={subIndex}
+                      as={Link}
+                      to={`/${subCategory.link}`}
+                    >
+                      {subCategory.name}
+                    </Dropdown.Item>
+                  ))}
+                </Dropdown.Menu>
+              </Dropdown>
+            ) : (
+              <Link
+                className="btn align-items-center category"
+                to={`/${category.link}`}
+              >
+                {category.name}
+              </Link>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default sideBar;
+export default SideBar;
