3c7e7a9d295068b50a49371d7dd57d5a9368065e
Rey Aldrin Uy <91858993+reyaldrinuy@users.noreply.github.com>
2024-03-26 02:53:06 -0400

    Add files via upload

--- frontend/src/components/ads/itemBlock.js
+++ frontend/src/components/ads/itemBlock.js
@@ -1,16 +1,20 @@
 import imageExample from './blacktshirt.png';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function itemBlock( {price, title, location} ) {
+function itemBlock( {id, price, title, location} ) {
 
-    { /* Remove these later and use variables above*/}
+    const handleClick = () => {
+        window.location.href =`/adDetails/${id}`;
+    };
+
     return (
         <div style={{ width:'300px', fontFamily:'Poppins'}}>
             { /* Replace image url with user uploaded img to be determined later*/}
-            <a href="#"><div className='imageHolder' style={{ width:'300px', height:'300px', backgroundImage:`url(${imageExample})`, backgroundSize:'cover', backgroundPosition:'center', backgroundColor:'#F5F5F5'}}></div></a>
+            <a href="#"><div className='imageHolder' style={{ width:'300px', height:'300px', backgroundImage:`url(${imageExample})`, backgroundSize:'cover', backgroundPosition:'center', backgroundColor:'#F5F5F5'}} onClick={handleClick}></div></a>
             <div className='pt-2'>
                 <h6 style={{ color:'#004C9B', fontWeight:'bold'}}>${price}</h6>
-                <a href="#" className="item-title" style={{ textDecoration:'none' }}><h5 style={{ color:'black', fontWeight:'bold'}}>{title}</h5></a>
+                <a href="#" className="item-title" style={{ textDecoration:'none' }}><h5 style={{ color:'black', fontWeight:'bold'}} onClick={handleClick}>{title}</h5></a>
                 <h6 style={{color:'#80A5CD'}}>{location}</h6>
             </div>
         </div>
