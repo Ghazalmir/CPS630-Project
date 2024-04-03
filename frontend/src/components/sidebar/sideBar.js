87b007368091cb1f38cff09fab33dac3f2077fbc
Rey Aldrin Uy <91858993+reyaldrinuy@users.noreply.github.com>
2024-04-03 06:12:05 -0400

    updated styling + functionality

--- frontend/src/components/sidebar/sideBar.js
+++ frontend/src/components/sidebar/sideBar.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { Link } from "react-router-dom"; 
 import "./sideBar.css";
 import Filters from './filters';
 import { useLocation } from 'react-router-dom';
 
 
 function SideBar() {
+  const [activeLink, setActiveLink] = useState(null);
   const [categories, setCategories] = useState([]);
   const [subCategories, setSubCategories] = useState([]);
   const [isCollapsed, setIsCollapsed] = useState(true);
   const location = useLocation();
+  const isAdminPanel = location.pathname.includes("/AdminPanel");
+  const isMyListings = location.pathname.includes("/MyListings"); 
 
   useEffect(() => {
     fetch("http://localhost:8080/api/categories/sections")
@@ -45,8 +48,46 @@ function SideBar() {
   return (
     <div className="container d-flex justify-content-center pt-5 side-bar">
       <ul className="list-unstyled">
-        <div className="categories-container"> 
-        {categories.map((category, index) => (
+      { !isAdminPanel && !isMyListings ? (
+        <div className="categories-container-title">
+          Categories
+        </div>
+      ) : (<></>)}
+        <div className="categories-container">
+        { isAdminPanel ? (
+          <>
+          <Link
+            className={`btn align-items-center category ${activeLink === '/AdminPanel/ReportedAds' ? 'active-link' : ''}`}
+            to="/AdminPanel/ReportedAds" onClick={() => setActiveLink('/AdminPanel/ReportedAds')}>
+              Reported Ads
+          </Link>
+          <Link
+            className={`btn align-items-center category ${activeLink === '/AdminPanel/ReportedUsers' ? 'active-link' : ''}`}
+            to="/AdminPanel/ReportedUsers" onClick={() => setActiveLink('/AdminPanel/ReportedUsers')}>
+              Reported Users
+          </Link>
+          <Link
+            className={`btn align-items-center category ${activeLink === '/AdminPanel/ManageUsers' ? 'active-link' : ''}`}
+            to="/AdminPanel/ManageUsers" onClick={() => setActiveLink('/AdminPanel/ManageUsers')}>
+              Manage Users
+          </Link>
+          </>
+        ) : ( isMyListings ? (
+          <>
+          <Link
+            className={`btn align-items-center category ${activeLink === '/MyListings' ? 'active-link' : ''}`}
+              to="/MyListings" onClick={() => setActiveLink('/MyListings')}>
+              My Listings
+          </Link>
+          <Link
+            className={`btn align-items-center category ${activeLink === '/MyListings' ? 'active-link' : ''}`}
+            to="/MyAccount">
+              Account Settings
+          </Link>
+          </>
+        ) : (
+          <>
+          {categories.map((category, index) => (
           <li key={index}>
             {checkSubCategory(category) ? (
                  <React.Fragment>
@@ -72,12 +113,14 @@ function SideBar() {
                   search: `selectedCategory=${category.category_id}&categoryName=${category.category_name}`,
                 }}>
                {category.category_name}
-          </Link>
-            )}
-          </li>
-        ))}
+              </Link>
+              )}
+            </li>
+            ))}
+          </>
+        ))}  
         </div>
-        <Filters />
+        { !isAdminPanel && !isMyListings ? (<Filters />) : (<></>)} 
       </ul>
     </div>
   );
